6eabf14edc96861b314ca5dc63c1aa44102a0a7e
Nilesh Kanawade <nilesh.kanawade@cloudjee.com>
2019-10-05 17:07:52 +0800

    Added user controller

--- src/main/java/com/sc/interview/crudapp/api/UserController.java
+++ src/main/java/com/sc/interview/crudapp/api/UserController.java
@@ -1,13 +1,64 @@
 package com.sc.interview.crudapp.api;
 
+import com.sc.interview.crudapp.dto.UserDto;
+import com.sc.interview.crudapp.exception.UserDoesNotExistException;
+import com.sc.interview.crudapp.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    public ResponseEntity get() {
-        return ResponseEntity.ok("Hello World!");
+    @Autowired
+    private UserService userService;
+
+    /**
+     * get all users with their addresses.
+     */
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> get() {
+        List<UserDto> userDtoList = this.userService.getAllUsersWithAddresses();
+        return ResponseEntity.ok(userDtoList);
+    }
+
+    /**
+     * create a user.
+     */
+    @PostMapping("/users")
+    public ResponseEntity<UserDto> create(@Valid @RequestBody UserDto userDto) {
+        UserDto userCreated = this.userService.create(userDto);
+        return ResponseEntity.ok(userCreated);
+    }
+
+    /**
+     * update a user.
+     */
+    @PutMapping("/users/{id}")
+    public ResponseEntity update(
+            @RequestParam(name = "id") final UUID id,
+            @Valid @RequestBody UserDto userDto) throws UserDoesNotExistException {
+
+        if (id != userDto.getId()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        UserDto userUpdated = this.userService.update(userDto);
+        return ResponseEntity.ok(userUpdated);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity delete(
+            @RequestParam(name = "id") final UUID id
+    ) throws UserDoesNotExistException {
+        this.userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
